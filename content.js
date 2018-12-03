29280f28f8e9c5259714641a767b544383567698
LabMemNo003 <LabMemNo003@gmail.com>
2018-12-03 16:14:13 +0800

    Reimplement content.js with Promise()

--- content.js
+++ content.js
@@ -110,32 +110,122 @@ async function wrapMatchedTextInNodes_(reObj, textNodeArray, wrapElem) {
         });
 }
 
-debugger;
 
-let reObj = /a/gi;
 
-let textNodeArray = [];
-let stack = [document];
-while (stack.length) {
-    let node = stack.pop();
 
-    if (node instanceof Text) {
-        textNodeArray.push(node);
-        continue;
-    }
 
-    let child = node.lastChild;
-    while (child) {
-        stack.push(child);
-        child = child.previousSibling;
-    }
+// **Parameters**
+// rootElem: The node under which the text nodes are found.
+// **Return values**
+// textNodeArray: The text nodes found under rotNode.
+async function getTextNodeArray(rootElem = document) {
+    return new Promise((resolve, _) => {
+        let textNodeArray = [];
+        let stack = [rootElem];
+        while (stack.length) {
+            let node = stack.pop();
+
+            if (node instanceof Text) {
+                textNodeArray.push(node);
+                continue;
+            }
+
+            if (node instanceof Element &&
+                node.classList.contains(LABEL.classWhiteList)) {
+                continue;
+            }
+
+            let child = node.lastChild;
+            while (child) {
+                stack.push(child);
+                child = child.previousSibling;
+            }
+        }
+        resolve(textNodeArray);
+    });
 }
 
-let wrapElem = document.createElement("spam");
-wrapElem.style.background = "green";
+const LABEL = {
+    classWhiteList: "lrwl",//"log_reader_white_list",
+}
 
-wrapMatchedTextInNodes(reObj, textNodeArray, wrapElem)
-    .then(countArray => {
-        let count = countArray.reduce((pre, cur) => pre + cur, 0);
-        alert(count);
-    })
+async function highlightText(reObj, color = "yellow", hint = undefined, link = undefined, className = undefined, rootElem = document) {
+
+    if (!className || !className.length) {
+        className = reObj.toString();
+    }
+
+    let wrapElem = document.createElement("a");
+    wrapElem.classList.add(className);
+    wrapElem.classList.add(LABEL.classWhiteList);
+    wrapElem.style.backgroundColor = color;
+
+    if (link && link.length) {
+        wrapElem.href = link;
+    }
+
+    let hintFlag, hintElement;
+    if (hint && hint.length) {
+        hintFlag = true;
+        hintElement = document.createElement("div");
+        hintElement.classList.add(LABEL.classWhiteList);
+        hintElement.innerHTML = hint;
+        document.body.append(hintElement);
+    }
+
+
+    return getTextNodeArray(rootElem)
+        .then(textNodeArray => {
+            return wrapMatchedTextInNodes(reObj, textNodeArray, wrapElem);
+        })
+        .then(countArray => {
+            let elements = document.getElementsByClassName(className);
+            for (let curNode of elements) {
+                if (hintFlag) {
+                    curNode.onmouseover = event => {
+                        hintElement.style.left = event.pageX + 1 + "px";
+                        hintElement.style.top = event.pageY + 1 + "px";
+                        hintElement.style.display = "block";
+                    }
+                    curNode.onmouseout = () => {
+                        hintElement.style.display = "none";
+                    }
+                }
+            }
+
+            let count = countArray.reduce((pre, cur) => pre + cur, 0);
+            console.log(reObj.toString(),count);
+            return count;
+        });
+}
+
+
+// Read all rules from local storage, and apply them to highlight the text.
+function triggerHighlightText() {
+
+    chrome.storage.local.get(
+        { rules: [] },
+        result => {
+            let promise = new Promise((resolve, _) => resolve(0));
+            for (let rule of result.rules) {
+                let reObj;
+                let flag = "g";
+                if (rule.isRegExp) {
+                    if (!rule.isCensitive) flag += "i";
+                    if (rule.isMultiline) flag += "m";
+                    reObj = new RegExp(rule.pattern, flag);
+                }
+                else {
+                    function escapeRegExp(string) {
+                        return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+                    }
+                    let pattern = escapeRegExp(rule.pattern);
+                    reObj = new RegExp(pattern, flag);
+                }
+                promise = promise.then(_ => {
+                    return highlightText(reObj, rule.color, rule.hint, rule.link);
+                });
+            }
+        }
+    )
+}
