bebda46768b603a10cc4266ec25f9b11b93c949d
LabMemNo003 <LabMemNo003@gmail.com>
2018-12-04 10:57:12 +0800

    Update options ui to include 'collapse and expand' function

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+chrome.storage.local.set({ version: "1.1.1" });
+
 chrome.commands.onCommand.addListener(function (command) {
     if (command == "trigger") {
         chrome.tabs.executeScript({ code: 'triggerHighlightText();' });
